63616ceea0cd0461e92e46fe9546e5b478442e76
이민규규ㄱ민미임이ㅇ <read1516@naver.com>
2023-01-24 22:50:35 +0900

    [소스정리]

--- src/solved/Main11.java
+++ src/solved/Main11.java
@@ -3,9 +3,6 @@ package solved;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 /* 숫자의 합 */
 public class Main11 {

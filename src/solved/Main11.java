fd623b8bcd3bc3feeeacd92c7ff5f906f82fae45
이민규규ㄱ민미임이ㅇ <read1516@naver.com>
2023-01-24 22:50:16 +0900

    [solved.ac] 숫자의합

---
+++ src/solved/Main11.java
@@ -0,0 +1,33 @@
+package solved;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/* 숫자의 합 */
+public class Main11 {
+
+	public static void main(String[] args) throws IOException {
+
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int size = Integer.parseInt(br.readLine());
+		String str = br.readLine();
+		
+		System.out.println(sum(str.toCharArray()));
+		
+		
+		
+	}
+	
+	private static int sum(char[] ch) {
+		int sum = 0;
+		for(char c : ch) {
+			sum += Character.getNumericValue(c);
+		}
+		return sum;
+	}
+}

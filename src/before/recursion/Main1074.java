62502326a799a0b631769c89011982dcce1334e6
이민규규ㄱ민미임이ㅇ <read1516@naver.com>
2025-03-22 18:22:20 +0900

    docs(백준): 재귀문제

---
+++ src/before/recursion/Main1074.java
@@ -0,0 +1,45 @@
+package before.recursion;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+//https://www.acmicpc.net/problem/1074
+public class Main1074 {
+
+    static int R,C;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] NRC = br.readLine().split(" ");
+
+        int N = Integer.parseInt(NRC[0]);
+        R = Integer.parseInt(NRC[1]);
+        C = Integer.parseInt(NRC[2]);
+        findLocation(N, C, R);
+        System.out.println(result);
+
+    }
+
+    static int result = 0;
+    private static void findLocation(int N, int col, int row) {
+        if (N == 0) {
+            return;
+        }
+
+        int size = 1<<N;
+        int half = size / 2;
+
+        if (col < half && row < half) {
+            findLocation(N - 1, col, row);
+        } else if (col >= half && row < half) {
+            result += half * half;
+            findLocation(N - 1, col-half, row);
+        } else if (col < half && row >= half) {
+            result += 2 * half * half;
+            findLocation(N - 1, col, row-half);
+        } else {
+            result += 3 * half * half;
+            findLocation(N - 1, col-half, row-half);
+        }
+    }
+}

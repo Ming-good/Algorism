fbd9f445190518b1df2a5fdd541a25ae8713b02d
이민규 <read1516@naver.com>
2025-03-28 17:09:18 +0900

    [백준] 그리디 문제

---
+++ src/before/greedy/Main12904.java
@@ -0,0 +1,18 @@
+package before.greedy;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+//https://www.acmicpc.net/problem/12904
+public class Main12904 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String S = br.readLine();
+        String T = br.readLine();
+
+
+
+
+    }
+}

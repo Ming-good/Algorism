2d81555fc2fc28f26868fb91a56c743b7a5d6848
이민규 <read1516@naver.com>
2025-03-28 15:27:08 +0900

    [백준] 그리디 문제

--- src/before/greedy/Main1946.java
+++ src/before/greedy/Main1946.java
@@ -2,43 +2,37 @@ package before.greedy;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.PriorityQueue;
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class Main1946 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[] passedPerson = new int[n];
-        for(int i=0;i<n;i++) {
-           int interviewSize =  Integer.parseInt(br.readLine());
-           PriorityQueue<Integer> queueA = new PriorityQueue<Integer>();
-           PriorityQueue<Integer> queueB = new PriorityQueue<Integer>();
 
-           for(int j=0;j<interviewSize;j++) {
-               String[] persons = br.readLine().split(" ");
-               queueA.add(Integer.parseInt(persons[0]));
-               queueB.add(Integer.parseInt(persons[1]));
-           }
-            passedPerson[i] = Math.max(interview(queueA), interview(queueB));
-        }
-
-        for(int person : passedPerson) {
-            System.out.println(person);
-        }
-
-    }
-
-    private static int interview(PriorityQueue<Integer> queue) {
-        int cnt = 0;
-        int personA = queue.poll();
-        while(!queue.isEmpty()) {
-            int personB = queue.poll();
-            if(personA < personB) {
-                cnt++;
+        for(int j=0;j<n;j++) {
+            int caseCnt = Integer.parseInt(br.readLine());
+            int[][] grade = new int[caseCnt][2];
+            for (int i = 0; i < caseCnt; i++) {
+                String[] str = br.readLine().split(" ");
+                grade[i][0] = Integer.parseInt(str[0]);
+                grade[i][1] = Integer.parseInt(str[1]);
             }
-        }
 
-        return cnt;
+            Arrays.sort(grade, (a, b) -> Integer.compare(a[0], b[0]));
+
+            int count = 1;
+            int bestInterviewScore = grade[0][1];
+            for (int i = 1; i < caseCnt; i++) {
+                if(grade[i][1] < bestInterviewScore) {
+                    bestInterviewScore = grade[i][1];
+                    count++;
+                }
+            }
+
+            System.out.println(count);
+        }
     }
+
 
 }

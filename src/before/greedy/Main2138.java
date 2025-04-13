19ec7cf5f1609002bfe7a43275eca1fd9c0de411
이민규규ㄱ민미임이ㅇ <read1516@naver.com>
2025-04-13 18:21:59 +0900

    docs(백준): 탐욕문제

--- src/before/greedy/Main2138.java
+++ src/before/greedy/Main2138.java
@@ -1,60 +1,47 @@
 package before.greedy;
 
 import java.io.*;
+import java.util.Arrays;
 
 public class Main2138 {
+    private static int min = Integer.MAX_VALUE;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        String from = br.readLine();
-        String to = br.readLine();
+        char[] from = br.readLine().toCharArray();
+        char[] to = br.readLine().toCharArray();
 
-        StringBuilder builder = new StringBuilder();
-        for(int i=1;i<from.length();i++) {
-            Integer fromCh1 = from.charAt(i-1)-48;
-            Integer toCh1 = to.charAt(i-1)-48;
-            if(fromCh1 == toCh1) {
-                builder.append(fromCh1);
-                continue;
-            }
+        char[] clone = from.clone();
+        clone[0] = (char) ('1'- clone[0]+'0');
+        clone[1] = (char) ('1'- clone[1]+'0');
+        processFlip(n, clone, to, 1);
+        processFlip(n, from, to, 0);
 
-            for(int j=-1;j<2&&i+j<from.length();j++) {
-                Integer light = from.charAt(i+j)-48;
-                if(builder.length()>=i+j+1) {
-                    builder.deleteCharAt(i+j);
-                    continue;
-                }
-                builder.append(1-light);
-            }
+        if (min == Integer.MAX_VALUE) {
+            System.out.println(-1);
+            return;
         }
 
-        System.out.println(builder);
+        System.out.println(min);
 
-        builder.setLength(0);
-        for(int i=1;i<from.length();i++) {
-            Integer fromCh1 = from.charAt(i-1)-48;
-            Integer toCh1 = to.charAt(i-1)-48;
-            if(fromCh1 == toCh1) {
-                builder.append(fromCh1);
-                continue;
-            }
-
-
-            for(int j=-1;j<2&&i+j<from.length();j++) {
-                if(i==1 && j==1) {
-                    break;
-                }
-
-                Integer light = from.charAt(i+j)-48;
-                if(builder.length()>=i+j+1) {
-                    builder.deleteCharAt(i+j);
-                    continue;
-                }
-                builder.append(1-light);
-            }
-        }
-
-
-        System.out.println(builder);
     }
+
+    private static void processFlip(int n, char[] from, char[] to, int flipCnt) {
+        for(int i=1;i< n;i++) {
+            if (from[i - 1] != to[i - 1]) {
+                flipCnt++;
+                flip(from, i);
+            }
+        }
+        if (Arrays.equals(from, to)) {
+            min = Math.min(min, flipCnt);
+        }
+    }
+
+    private static void flip(char[] arr, int num) {
+        for (int i = -1; i < 2 && i+num < arr.length; i++) {
+            arr[i + num] = (char) ('1' - arr[i + num] + '0');
+        }
+    }
+
 }

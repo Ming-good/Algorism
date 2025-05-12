0a92ea71bd44b2e80476f0e558664432c4b67b1a
Ming-good <read1516@naver.com>
2025-05-12 23:56:20 +0900

    docs(백준): 그리디 도서관 재풀이

--- src/bakjun/greedy/Main1461_2.java
+++ src/bakjun/greedy/Main1461_2.java
@@ -15,11 +15,11 @@ public class Main1461_2 {
         int n = Integer.parseInt(tokken[0]);
         int m = Integer.parseInt(tokken[1]);
 
-        String[] bookTokken = br.readLine().split(" ");
+        String[] bookToken = br.readLine().split(" ");
         List<Integer> minusList = new ArrayList();
         List<Integer> plusList = new ArrayList<>();
-        int sumDistance = 0;
-        for (String book : bookTokken) {
+        int totalDistance = 0;
+        for (String book : bookToken) {
             int num = Integer.parseInt(book);
             if (num < 0) {
                 minusList.add(num*-1);
@@ -28,22 +28,25 @@ public class Main1461_2 {
             plusList.add(num);
         }
 
-        if (minusList.isEmpty()) minusList.add(0);
-        if (plusList.isEmpty()) plusList.add(0);
-
         Collections.sort(minusList, Collections.reverseOrder());
         Collections.sort(plusList, Collections.reverseOrder());
-        for (int i = 0; i < minusList.size();i++) {
-            sumDistance += minusList.get(i)*2;
-            i += m - 1;
-        }// 39 37 29 28 6 0 2 11
 
-        for (int i = 0; i < plusList.size();i++) {
-            sumDistance += plusList.get(i)*2;
-            i += m - 1;
-        }
-        sumDistance -= Math.max(minusList.get(0), plusList.get(0));
-        System.out.println(sumDistance);
+        totalDistance += calcuateDistance(minusList, m);
+        totalDistance += calcuateDistance(plusList, m);
+
+        int maxDist = 0;
+        if (!minusList.isEmpty()) maxDist = Math.max(maxDist, minusList.get(0));
+        if (!plusList.isEmpty()) maxDist = Math.max(maxDist, plusList.get(0));
+        totalDistance -= maxDist;
+
+        System.out.println(totalDistance);
     }
 
+    private static int calcuateDistance(List<Integer> list, int m) {
+        int distance=0;
+        for (int i = 0; i < list.size();i+=m) {
+            distance += list.get(i)*2;
+        }
+        return distance;
+    }
 }
